c86218e9140f033d6ff87ac91ff2fda0848ec4f9
Timofei197 <107853533+Timofei197@users.noreply.github.com>
2022-06-21 00:38:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte degrees = 15; // градусов на улице
+            sbyte kat = 1; // есть одна кошка
+            short number = 20; // число месяца
+            ushort age = 22; // мой возраст
+            int Monday = 1;// первый день недели
+            float pi1 = 3,1415926;// число пи до 7 знака после запятой
+            double pi2 = 3,141592653589793; // число пи до 15 знака после запятой
+            char sign = '!';// просто символ
+            string weather = "холодно"; // погода на улице
+            bool bl = 5;
+        }
+    }
+}
