20f74f6def8f21539069a6901f68d923ebceee15
Timofei197 <107853533+Timofei197@users.noreply.github.com>
2022-06-21 15:16:40 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,15 +11,15 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             byte degrees = 15; // градусов на улице
-            sbyte kat = 1; // есть одна кошка
+            sbyte cat = 1; // есть одна кошка
             short number = 20; // число месяца
             ushort age = 22; // мой возраст
-            int Monday = 1;// первый день недели
+            int monday = 1;// первый день недели
             float pi1 = 3,1415926;// число пи до 7 знака после запятой
             double pi2 = 3,141592653589793; // число пи до 15 знака после запятой
-            char sign = '!';// просто символ
+            char sign = '!';// восклицательный знак 
             string weather = "холодно"; // погода на улице
-            bool bl = 5;
+            bool adult = age >= 18; //если тыой возраст от 18 лет и страше то ты совершеннолетний 
         }
     }
 }
