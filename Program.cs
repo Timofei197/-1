1ad6156be5c00e7ed9d158045055ae42ef5dbe22
Timofei197 <107853533+Timofei197@users.noreply.github.com>
2022-06-22 16:54:23 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,21 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace задание__3
+namespace Задание__4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите ваше имя "); 
-            string name  = Console.ReadLine (); 
-            Console.Write("Сколько вам лет? "); 
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Где вы живете? "); 
-            string city = Console.ReadLine ();
-            Console.Write("На кого учитесь? "); 
-            string profession = Console.ReadLine ();
-            Console.WriteLine("Вас зовут " + name + " вам " + age + " года, живете в городе " + city + " и учитесь на " + profession + ".");
+            int images = 52; 
+            int imagersInARow = 3;
+            int quantityOfRow = images / imagersInARow;
+            Console.WriteLine("Сколько всего заполненных рядов " + quantityOfRow);
+            int excessImages = images - quantityOfRow * imagersInARow; 
+            Console.WriteLine("Картинок лишних " + excessImages);
         }
     }
 }
