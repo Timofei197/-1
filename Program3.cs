29153e4e2bf350428ffe6976e0bd0ee979332c66
Timofei197 <107853533+Timofei197@users.noreply.github.com>
2022-06-24 02:01:19 +0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int crystals = 14;
+            int crystalPrice = 50;
+            Console.Write("Сколько у вас золота? ");
+            int gold = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько кристаллов хотите купить? ");
+            int crystalsToBuy = Convert.ToInt32(Console.ReadLine());
+            crystals = crystalsToBuy + crystals;
+            gold = gold - crystalsToBuy * crystalPrice;
+            Console.WriteLine("У вас осталось " + gold + " золота");
+            Console.WriteLine("У вас осталось " + crystals + " кристаллов");
+        }
+    }
+}
