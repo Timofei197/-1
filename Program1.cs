793cdc206b36e7c56a955318b5ceb97e5fd6b8f0
Timofei197 <107853533+Timofei197@users.noreply.github.com>
2022-06-24 21:01:27 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Гудовщиков";
+            string surname = "Тимофей";
+            Console.WriteLine("Ваше имя " + name + " ваша фамилия " + surname);
+            (name, surname) = (surname, name);
+            Console.WriteLine("Ваше имя " + name + " ваша фамилия " + surname);
+        }
+    }
+}
