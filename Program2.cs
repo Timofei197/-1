e161cf5f890030c772d217f704b041b8f8bca836
Timofei197 <107853533+Timofei197@users.noreply.github.com>
2022-06-25 18:33:06 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Сколько раз вы хотите выполнить программу? ");
+            int numberOfRepetitions = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Какое сообщение хотите вывести? ");
+            string message = Console.ReadLine();
+            while (numberOfRepetitions-- > 0)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
