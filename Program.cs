8a9ffbe45ac26989b124bfdcb0127eb613ac1229
Timofei197 <107853533+Timofei197@users.noreply.github.com>
2022-06-26 02:08:38 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,19 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Очередь
+namespace последовательность
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int timeForOnePerson = 10;
-            int hour = 60;
-            Console.Write("Сколько человек в очереди? ");
-            int peopleInLine = Convert.ToInt32(Console.ReadLine());
-            int waitingHours = timeForOnePerson * peopleInLine / hour;
-            int minutesOfWaiting = timeForOnePerson * peopleInLine - hour * waitingHours;
-            Console.WriteLine("Время ожидания в очереди - " + waitingHours + " часов " + minutesOfWaiting + " минут");
+            for (int i = 5; i <=100; i = i + 7)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
