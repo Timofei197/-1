9bd033010c49e5e51aea39a05b79477ad0439ae3
Timofei197 <107853533+Timofei197@users.noreply.github.com>
2022-06-26 18:26:41 +0300

    Add files via upload

---
+++ Program8.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace рандом
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number; 
+            int result = 0;
+            int upperLimit = 100;
+            int lowerLimit = 3;
+            int divider = 5;
+            Random random = new Random();
+            number = random.Next(lowerLimit, upperLimit);
+            Console.WriteLine(number);
+
+            for (int i = number; i != 0; i--)
+            {
+                if(i % lowerLimit == 0 || i % divider == 0)
+                {
+                    result += i;
+                    Console.WriteLine(i);
+
+                    if (i % lowerLimit == 0 && i % divider == 0)
+                    {
+                        result += i;
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
