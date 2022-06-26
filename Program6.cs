729ecce01475dd2e76eee19d07d5d6d470d047e3
Timofei197 <107853533+Timofei197@users.noreply.github.com>
2022-06-26 03:46:31 +0300

    Add files via upload

---
+++ Program6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace рандом
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, itog = 0;
+            Random rand = new Random();
+            number = rand.Next(3, 100);
+            Console.WriteLine(number);
+            for (int i = number; i != 0; i--)
+            {
+                if(i%3 == 0)
+                {
+                    itog = itog + i;
+                }
+            }
+            itog = itog + number;
+            Console.WriteLine(itog);
+        }
+    }
+}
