e41d32874a7ad8977de60308a71cfab8909f450e
Timofei197 <107853533+Timofei197@users.noreply.github.com>
2022-07-04 13:32:13 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,20 +4,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace последовательность
+namespace конвертер_валют
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int upperLimit = 96;
-            int lowerLimit = 5;
-            int valueIteration = 7;
+            double balanceRUB = 1000;
+            double balanceUSD = 1000;
+            double balanceEUR = 1000;
+            double courseRUBUSD = 0,01859;
+            double courseRUBEUR = 0,01774;
+            double courseUSDRUB = 53,77;
+            double courseEURRUB = 56,36;
+            Console.WriteLine("У вас на балансе " + balanceRUB + " рублей " + balanceUSD + " долларов " + balanceEUR + " евро ");
+            Console.Write("Какую валюту вы хотите купить? ");
+            string buyCurrency = Console.ReadLine();
+            Console.Write("Какой валютой хотите оплатить? ");
+            string saleCurrency = Console.ReadLine();
+            Console.Write("Сколько валюты хотите купить? ");
+            int quantityСurrencies = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = lowerLimit; i <= upperLimit; i += valueIteration)
+            switch (buyCurrency)
             {
-                Console.WriteLine(i);
+                case "Рубли":
+
+                    switch (saleCurrency)
+                    {
+                        case "Доллары":
+                            balanceRUB = quantityСurrencies + balanceRUB;
+                            balanceUSD = balanceUSD - quantityСurrencies * courseRUBUSD;
+                            break;
+                        case "Евро":
+                            balanceRUB = quantityСurrencies + balanceRUB;
+                            balanceEUR = balanceEUR - quantityСurrencies * courseEURRUB;
+                            break;
+                    }
+                    break;
+                case "Доллары":
+
+                    switch (saleCurrency)
+                    {
+                        case "Рубли":
+                            balanceUSD = quantityСurrencies + balanceUSD;
+                            balanceRUB = balanceRUB - quantityСurrencies * courseUSDRUB;
+                            break;
+                        case "Евро":
+                            balanceUSD = quantityСurrencies + balanceUSD;
+                            balanceEUR = balanceEUR - quantityСurrencies * courseUSDRUB;
+                            break;
+                    }
+                    break;
+                case "Евро":
+                    switch (saleCurrency)
+                    {
+                        case "Рубли":
+                            balanceEUR = quantityСurrencies + balanceEUR;
+                            balanceRUB = balanceEUR - quantityСurrencies * courseEURRUB;
+                            break;
+                        case "Доллары":
+                            balanceEUR = quantityСurrencies + balanceEUR;
+                            balanceUSD = balanceUSD - quantityСurrencies * courseEURUSD;
+                            break;
+                    }
+                    break;
             }
+            Console.WriteLine("У вас на балансе " + balanceRUB + " рублей " + balanceUSD + " долларов " + balanceEUR + " евро ");
         }
     }
 }
